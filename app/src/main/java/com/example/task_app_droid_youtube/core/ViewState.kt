e38e25a34964e2c2ef58da25fecedcc4958d3e76
Habibi Coding <habibicoding@aol.com>
2023-07-30 17:38:15 +0200

    Add core package.

---
+++ app/src/main/java/com/example/task_app_droid_youtube/core/ViewState.kt
@@ -0,0 +1,16 @@
+package com.example.task_app_droid_youtube.core
+
+
+sealed class ViewState<out T : Any> {
+
+    object Loading : ViewState<Nothing>()
+    data class Success<out T : Any>(val data: T) : ViewState<T>()
+    data class Error(val exception: Exception) : ViewState<Nothing>()
+
+    val extractData: T?
+        get() = when (this) {
+            is Success -> data
+            is Error -> null
+            Loading -> null
+        }
+}
