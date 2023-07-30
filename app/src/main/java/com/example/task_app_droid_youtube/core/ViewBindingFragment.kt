cf6c192c237aa5a138621abb3fe3a3e970400c0d
Habibi Coding <habibicoding@aol.com>
2023-07-30 17:41:45 +0200

    Add core package.

---
+++ app/src/main/java/com/example/task_app_droid_youtube/core/ViewBindingFragment.kt
@@ -0,0 +1,21 @@
+package com.example.task_app_droid_youtube.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class ViewBindingFragment<VB : ViewBinding> : Fragment() {
+
+    protected lateinit var binding: VB
+
+    abstract fun createBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = createBinding(inflater, container).also { binding = it }.root
+}
