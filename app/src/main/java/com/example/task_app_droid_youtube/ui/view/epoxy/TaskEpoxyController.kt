5415c649d238d31ac8d72c1ec5cfb41735d173fb
Habibi Coding <habibicoding@aol.com>
2023-07-30 22:02:43 +0200

    Add util package and logic for Epoxy.

--- app/src/main/java/com/example/task_app_droid_youtube/ui/view/epoxy/TaskEpoxyController.kt
+++ app/src/main/java/com/example/task_app_droid_youtube/ui/view/epoxy/TaskEpoxyController.kt
@@ -1,10 +1,30 @@
 package com.example.task_app_droid_youtube.ui.view.epoxy
 
 import com.airbnb.epoxy.EpoxyController
+import com.example.task_app_droid_youtube.itemTask
+import com.example.task_app_droid_youtube.model.TaskFetchResponse
 
-class TaskEpoxyController(private val cardBackgroundColor: Int): EpoxyController() {
+class TaskEpoxyController(private val cardBackgroundColor: Int) : EpoxyController() {
+
+    private val tasks = mutableListOf<TaskFetchResponse>()
 
     override fun buildModels() {
-        TODO("Not yet implemented")
+        this.tasks.forEachIndexed { _, model ->
+            itemTask {
+                id(model.id)
+                taskResponse(model)
+                bgColor(this@TaskEpoxyController.cardBackgroundColor)
+            }
+        }
     }
+
+    fun setTasks(items: List<TaskFetchResponse>) {
+        this.tasks.clear()
+        this.tasks.addAll(items)
+        requestModelBuild()
+    }
+
+    fun getNumberOfTasks(): Int = this.tasks.size
+
+    fun getTaskById(index: Int): TaskFetchResponse = this.tasks[index]
 }
