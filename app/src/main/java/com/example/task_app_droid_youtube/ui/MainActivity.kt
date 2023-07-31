59327b805be5ce0bda073805f4229dff62952647
Habibi Coding <habibicoding@aol.com>
2023-07-31 16:37:37 +0200

    Finish MainActivity.

--- app/src/main/java/com/example/task_app_droid_youtube/ui/MainActivity.kt
+++ app/src/main/java/com/example/task_app_droid_youtube/ui/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.task_app_droid_youtube.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.task_app_droid_youtube.R
+import com.example.task_app_droid_youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.closed_tasks,
+                R.id.open_tasks,
+                R.id.all_tasks
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
