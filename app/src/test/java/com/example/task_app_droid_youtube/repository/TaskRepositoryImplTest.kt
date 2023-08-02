31b01adcf8189551c398cc74c7115267589f438c
Habibi Coding <habibicoding@aol.com>
2023-08-02 14:16:35 +0200

    Add remaining Unit Tests.

--- app/src/test/java/com/example/task_app_droid_youtube/repository/TaskRepositoryImplTest.kt
+++ app/src/test/java/com/example/task_app_droid_youtube/repository/TaskRepositoryImplTest.kt
@@ -10,6 +10,8 @@ import com.example.task_app_droid_youtube.model.TaskUpdateRequest
 import com.example.task_app_droid_youtube.networking.TaskApi
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import io.mockk.MockKException
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -19,7 +21,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
-import org.junit.Before
 import org.junit.Test
 
 internal class TaskRepositoryImplTest : BaseRepositoryTest() {
@@ -57,7 +58,7 @@ internal class TaskRepositoryImplTest : BaseRepositoryTest() {
     private val mockTaskApi = mockk<TaskApi>()
     private val mockRepository = TaskRepositoryImpl(mockTaskApi)
 
-    @Before
+
     override fun setUp() {
         super.setUp()
 
@@ -170,6 +171,139 @@ internal class TaskRepositoryImplTest : BaseRepositoryTest() {
             assertTrue(actualResult is ViewState.Error)
             val actualMessage = (actualResult as ViewState.Error).exception.message
             assertEquals(customException.message, actualMessage)
+        }
+    }
+
+    @Test
+    fun `when fetching all tasks then check for success response`() {
+        // given
+        val jsonArray = getJsonString<List<TaskFetchResponse>>(TASKS_RESPONSE)
+        val listType = object : TypeToken<ArrayList<TaskFetchResponse?>?>() {}.type
+        val expectedItems = gsonBuilder.fromJson<List<TaskFetchResponse>>(jsonArray, listType)
+
+        mockWebServer.enqueue(MockResponse().setBody(FileReader(TASKS_RESPONSE).content))
+
+        runBlocking {
+            // when
+            val actualResult = objectUnderTest.getTasks(null).extractData
+            // then
+            assertEquals(expectedItems, actualResult)
+        }
+    }
+
+    @Test
+    fun `when fetching all tasks then then check for http exception`() {
+        // given
+        mockWebServer.enqueue(MockResponse().setResponseCode(INTERNAL_SERVER_ERROR))
+
+        runBlocking {
+            // when
+            val actualResult: ViewState<List<TaskFetchResponse>> = objectUnderTest.getTasks(null)
+
+            // then
+            assertTrue(actualResult is ViewState.Error)
+            val actualMessage = (actualResult as ViewState.Error).exception.message
+            assertEquals(SOMETHING_WRONG, actualMessage)
+        }
+    }
+
+    @Test
+    fun `when fetching all tasks then then check for unknown exception`() {
+        // given
+        coEvery { mockRepository.getTasks(null) } throws customException
+
+        runBlocking {
+            // when
+            val actualResult: ViewState<List<TaskFetchResponse>> = mockRepository.getTasks(null)
+
+            // then
+            assertTrue(actualResult is ViewState.Error)
+            val actualMessage = (actualResult as ViewState.Error).exception.message
+
+            assertEquals(customException.message, actualMessage)
+        }
+    }
+
+    @Test
+    fun `when patch task request sent then check for success response`() {
+        // given
+        val expectedTask = getDataClass<TaskFetchResponse>(TASK_PATCH_REQUEST)
+        mockWebServer.enqueue(MockResponse().setBody(FileReader(TASK_PATCH_REQUEST).content))
+
+        runBlocking {
+            // when
+            val actualTask = objectUnderTest.updateTask("2", updateRequest).extractData
+            // then
+            assertEquals(expectedTask, actualTask)
+        }
+    }
+
+    @Test
+    fun `when patch task request sent then check for http exception`() {
+        // given
+        mockWebServer.enqueue(MockResponse().setResponseCode(INTERNAL_SERVER_ERROR))
+
+        runBlocking {
+            // when
+            val actualResult = objectUnderTest.updateTask("19", updateRequest)
+
+            // then
+            assertTrue(actualResult is ViewState.Error)
+            assertEquals(SOMETHING_WRONG, (actualResult as ViewState.Error).exception.message)
+        }
+    }
+
+    @Test
+    fun `when patch task request sent then check for unknown exception`() {
+        // given
+        coEvery { mockRepository.updateTask(any(), any()) } throws customException
+
+        runBlocking {
+            // when
+            val actualResult = mockRepository.updateTask("111", updateRequest)
+
+            // then
+            assertTrue(actualResult is ViewState.Error)
+            assertEquals(
+                customException.message,
+                (actualResult as ViewState.Error).exception.message
+            )
+        }
+    }
+
+    @Test
+    fun `when delete task request sent then check for successful removal`() {
+        mockWebServer.enqueue(MockResponse().setResponseCode(204))
+
+        runBlocking {
+            val actualMessage = objectUnderTest.canDeleteTask("15").extractData
+            val isSuccessful: Boolean = actualMessage?.isSuccessful ?: false
+            assertEquals(true, isSuccessful)
+        }
+    }
+
+    @Test
+    fun `when delete task request sent then check for http exception`() {
+        mockWebServer.enqueue(MockResponse().setResponseCode(INTERNAL_SERVER_ERROR))
+
+        runBlocking {
+            val actualResult = objectUnderTest.canDeleteTask("2")
+
+            assertTrue(actualResult is ViewState.Error)
+            assertEquals(SOMETHING_WRONG, (actualResult as ViewState.Error).exception.message)
+        }
+    }
+
+    @Test
+    fun `when delete task request sent then check for unknown exception`() {
+        val mockException = MockKException("mocked exception")
+        coEvery { mockRepository.canDeleteTask(any()) } throws mockException
+
+        runBlocking {
+            val actualResult = mockRepository.canDeleteTask("23")
+
+            assertTrue(actualResult is ViewState.Error)
+            assertEquals(mockException::class, (actualResult as ViewState.Error).exception::class)
         }
     }
 }
